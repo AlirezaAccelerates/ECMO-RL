a9f77eced641b6058de57a7801fca1d7d445e858
Alireza <96019816+AlirezaRafiei9@users.noreply.github.com>
2022-06-30 18:26:57 +0430

    Update Q_learning.py

--- Q_learning.py
+++ Q_learning.py
@@ -20,6 +20,31 @@ action_set = {                                                     # Initializin
 
 ################################################################# Creating Q function
 # It can be either a trainable or nontrainable model - here I simply put a nueral network
+
+class Approximation_Function():
+    def __init__(self):
+        
+        self.models = []
+        for i in range(#number of actions):
+            model = SGDRegressor(learning_rate="constant")
+            model.partial_fit([state_], [0]) #Initial state
+            self.models.append(model)
+            
+    def predict(self, s, a=None):
+        
+        s_ = self.s
+        if not a:
+            return np.array([m.predict([s_])[0] for m in self.models])
+        else:
+            return self.models[a].predict([s_])[0]
+        
+    def update(self, s, a, y):
+       
+        s_update = self.s
+        self.models[a].partial_fit([s_update], [y])
+    
+    
+    
     
 l1 = 32                                                        
 l2 = 64
@@ -40,14 +65,14 @@ epochs = 1000
 losses = [] 
 epsilon = 1.0
 gamma = 0.9
-for i in range(epochs):                                         
-    ECMO = OxygenChallengeEnv(#possible initializations)        # Choosing a patient
-    state_ = ECMO.states                                        # The initial states for a patient
-    state1 = torch.from_numpy(state_).float()                   # Converting numpy array to PyTorch variable
+for i in range(epochs): 
+    estimator = Approximation_Function()
+    ECMO = OxygenChallengeEnv(#possible initializations)        # Choosing a patient and reset the environments
+    state1 = ECMO.states                                        # The initial states for a patient
     status = 1                                                  # Keeping track the processing
     while(status == 1):                                         # Doing ECMO
-        qval = model(state1)                                    # Determining Q value - We should pay attention to the size of variables
-        qval_ = qval.data.numpy()
+        qval = estimator.predict(state1)                        # Determining Q value - We should pay attention to the size of the variables
+        qval_ = qval.data.numpy()                               # Maybe we do not need this!
         
 ################################################################# Adding epsilon-greedy method
         if (random.random() < epsilon): 
@@ -58,13 +83,12 @@ for i in range(epochs):
         
         action = action_set[action_]                            #Choosing the best action
         ECMO.DoAction(action) 
-        state2_ = ECMO.states 
-        state2 = torch.from_numpy(state2_).float()
+        state2 = ECMO.states 
         reward = ECMO.reward()
         
-        with torch.no_grad():                                   # As we don't want any learning here. Just updating
-            newQ = model(state2)
-        maxQ = torch.max(newQ)                                  # Finding the maximum Q value predicted from the new state
+        newQ = estimator.predict(state2)
+        td_target = reward + gamma * np.max(newQ)
+        maxQ = np.argmax(newQ)                                  # Finding the maximum Q value predicted from the new state
         
         if reward == ? and ! and ...:                           # Is it the last iteration or not? After we define the reward fuction, we should motify this
             Y = reward + (gamma * maxQ)
